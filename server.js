bd4ac52320eabbd0fa454a8bca20ac1d16bb1b93
Rodney P <rodney.plancher@gmail.com>
2022-02-14 17:10:09 -0500

    added sql and a query to select all

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const mysql = require("mysql2");
 const PORT = process.env.PORT || 3301;
 const app = express();
 
@@ -12,6 +12,24 @@ app.get("/", (req, res) => {
   });
 });
 
+// default reponse for any other not found
+app.use((req, res) => {
+  res.status(404).end();
+});
+//  conecting to sql
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "1100",
+    database: "election",
+  },
+  console.log("connected to election database")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
